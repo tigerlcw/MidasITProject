cd2cc9bb5a071d519913f271f78ff1efc9b9ede2
stealstick <stillstick40@gmail.com>
2017-05-27 20:06:02 +0900

    Fix URL

--- ohmyroutine/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^routine_detail/(?P<routineid>.+)/$', views.routine_detail),
-    url(r'^timer/$', views.exercise_timer),
-]
